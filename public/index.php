18a594db9b0a035006c06f4692685d21f481cb6d
Tony Condone <touyboateng339@gmail.com>
2025-08-09 10:37:03 +0000

    auto: Sat, Aug  9, 2025 10:37:03 AM

--- public/index.php
+++ public/index.php
@@ -172,6 +172,7 @@ $routes = [
     '/admin/webhooks' => 'App\Controllers\AdminPortalController@webhooks',
     '/admin/system-monitoring' => 'App\Controllers\AdminPortalController@systemMonitoring',
     '/admin/audit/overrides' => 'App\\Controllers\\AdminAuditController@overrides',
+    '/admin/audit/schema' => 'App\\Controllers\\AdminAuditController@schema',
     
     // Application routes
     '/applications' => 'App\Controllers\ApplicationController@index',
